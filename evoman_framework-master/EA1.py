1a2d6c284ed1c347ced85ff46d611b605bfed064
Robino1997 <Robin@MacBook-Air-van-Robin.local>
2019-09-16 11:38:23 +0200

    Random init of pupulation

--- evoman_framework-master/EA1.py
+++ evoman_framework-master/EA1.py
@@ -2,7 +2,9 @@ import sys
 sys.path.insert(0, 'evoman')
 from environment import Environment
 from demo_controller import player_controller
+import numpy as np
 
+import time
 import glob, os
 
 experiment_name = 'algorithm_one'
@@ -16,4 +18,40 @@ env = Environment(experiment_name=experiment_name,
                   player_controller=player_controller(),
                   enemymode="static",
                   level=2,
-                  speed="fastest")
+                  speed='fastest')
+
+# default environment fitness is assumed for experiment
+
+env.state_to_log() # checks environment state
+
+
+####   Optimization for controller solution (best genotype-weights for phenotype-network): Ganetic Algorihm    ###
+
+ini = time.time()  # sets time marker
+
+
+# genetic algorithm params
+
+run_mode = 'train' # train or test
+
+n_hidden = 10
+n_vars = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5 # multilayer with 10 hidden neurons
+dom_u = 1
+dom_l = -1
+npop = 10
+gens = 30
+mutation = 0.2
+last_best = 0
+
+np.random.seed(112)
+
+#Play game for 10 population size, random init
+pop = np.random.uniform(1, -1, (npop,n_vars))
+
+def simulate(env,x):
+    f,p,e,t = env.play(pcont=x)
+    return f
+
+fit_pop = np.array(list(map(lambda y: simulate(env, y), pop)))
+
+print(fit_pop)
