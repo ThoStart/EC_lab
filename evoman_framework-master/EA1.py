1bbf42dd58e24258b398985f02889efd698a6feb
Robin1997 <robinopdam@hotmail.com>
2019-09-17 12:56:44 +0200

    create branch robin

--- evoman_framework-master/EA1.py
+++ evoman_framework-master/EA1.py
@@ -10,6 +10,8 @@ from demo_controller import player_controller
 from deap import base, creator, tools
 
 
+########################### Init Environment ############################
+
 experiment_name = 'algorithm_one'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
@@ -39,35 +41,81 @@ n_hidden = 10
 n_vars = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5  # multilayer with 10 hidden neurons
 dom_u = 1
 dom_l = -1
-npop = 2
-gens = 30
+npop = 30
+gens = 20
 mutation = 0.2
+cxpb = 0.2
 last_best = 0
+indpb = 0.2
+tournsize = 5
 
 np.random.seed(112)
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
 
-toolbox = base.Toolbox()
-toolbox.register("attr_float", random.uniform, -1, 1)
-toolbox.register("individual", tools.initRepeat, creator.Individual,
-                 toolbox.attr_float, n=n_vars)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-pop = toolbox.population(n=npop)
-
+########################### Init Tools and Functions ############################
 
 def evaluate(individual):
     f, p, e, t = env.play(pcont=np.array(individual))
     return f,
 
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
 
+toolbox = base.Toolbox()
+toolbox.register("attr_float", random.uniform, dom_l, dom_u)
+toolbox.register("individual", tools.initRepeat, creator.Individual,
+                 toolbox.attr_float, n=n_vars)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("evaluate", evaluate)
 
-fitnesses = list(map(toolbox.evaluate, pop))
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=indpb)
+toolbox.register("select", tools.selTournament, tournsize=tournsize)
 
+
+########################### Init Population ############################
+
+pop = toolbox.population(n=npop)
+
+fitnesses = list(map(toolbox.evaluate, pop))
 for ind, fit in zip(pop, fitnesses):
     ind.fitness.values = fit
 
-print('second individual fitness value: ', pop[1].fitness)
+for g in range(gens):
+    print("Generation: ", g)
+    offspring = toolbox.select(pop, len(pop)) # we can vary tournament size here
+    offspring = list(map(toolbox.clone, offspring))
+
+    print("Recombination")
+    for child1, child2 in zip(offspring[::2], offspring[1::2]):
+        if random.random() < cxpb:
+            toolbox.mate(child1, child2)
+            del child1.fitness.values
+            del child2.fitness.values
+
+    print("Mutate")
+    for mutant in offspring:
+        if random.random() < mutation:
+            toolbox.mutate(mutant)
+            del mutant.fitness.values
+
+    print("Evaluate")
+
+    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+    fitnesses = map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    pop[:] = offspring
+
+    fits = [ind.fitness.values[0] for ind in pop]
+
+    length = len(pop)
+    mean = sum(fits) / length
+    sum2 = sum(x * x for x in fits)
+    std = abs(sum2 / length - mean ** 2) ** 0.5
+
+    print("  Min %s" % min(fits))
+    print("  Max %s" % max(fits))
+    print("  Avg %s" % mean)
+    print("  Std %s" % std)
